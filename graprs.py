bc07c02b3459ca621cf418a8489c86febcc8501f
Dariusz <113003510+thelemic@users.noreply.github.com>
2022-09-10 18:37:04 +0200

    Add files via upload

--- graprs.py
+++ graprs.py
@@ -1,15 +1,18 @@
 import random
+import sys
+
+player_points = 0
+computer_points = 0
 
 while(True):
         print("===========Prepare to battle!!!==============")
-        user_hand = input("Choose paper, rock or scissors: ").lower()
-        possibilities = ["paper", "rock", "scissors"]
+        user_hand = input("Choose paper, rock, scissors or exit to quit the program: ").lower()
+        possibilities = ["paper", "rock", "scissors", "exit"]
         computer_hand = random.choice(possibilities)
         if user_hand not in possibilities:
                 print("FOLLOW THE RULES STUPID!!! Paper, rock or scissors only!")
 
-        player_points = 0
-        computer_points = 0
+        
 
         if user_hand == computer_hand:
                 print("That's draw! No points!")
@@ -37,13 +40,14 @@ while(True):
                         print("Looser!!! Comp choose:" + computer_hand + "!")
                         computer_points += 1
 
+        elif user_hand == "exit":
+                print("Run Chicken, run!!!")
+                sys.exit()
+
         print("Computer points: ",computer_points,"\n" "Player points: ", player_points,)
         if computer_points > player_points:
                 print("This time Computer win!")
         elif computer_points < player_points:
                 print("Player wins!!!")
-        play_again = input("Want some more? y/n: ")
-        if play_again.lower() != "y":
-                print("Run Chicken, run!!!")
-                break
+        
         
